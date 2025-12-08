8a15c71be57f4a0ca0401e24e4d6dec59c13b291
kpihlak <kartpihlak@hkhk.edu.ee>
2025-12-08 11:27:47 +0200

    Add files via upload

---
+++ kodune ül.py
@@ -0,0 +1,47 @@
+# 2.1 Äratus
+
+
+# kordadearv = int(input("Kordade arv: "))
+# for i in range(kordadearv):
+#     print("Tõuse ja sära!")
+
+
+# 2.2 Murelikud lapsevanemad
+
+# porgandid = 0
+# ringide_arv = 6
+
+
+# while ringide_arv > 0:
+#     #print(ringide_arv)
+#     if ringide_arv % 2 == 0:
+#         porgandid += ringide_arv
+#     ringide_arv-=1
+    
+
+# print(porgandid)
+
+# 2.5 Õunad
+
+# Lumivalgekesel oli 14 õuna ja ta tahtis neid pöialpoistega jagada. Ta sai aru, et kui kõik seitse pöialpoissi tahavad õunu ja ta annaks kõigile kaks õuna, jääks ta ise üldse ilma. Nüüd otsustas ta õunu jagada nii, et küsib, mitu pöialpoissi tahab õunu, ja seejärel loosib iga soovija korral, kas anda talle üks või kaks õuna. Koostada programm, mis
+# küsib kasutajalt, mitu pöialpoissi tahab õunu (võib eeldada, et sisestatakse täisarv lõigust [0; 7]);
+# leiab ja väljastab eraldi ridadele, mitu õuna saab iga pöialpoiss (programm genereerib iga kord juhuslikult arvu 1 või 2);
+# leiab ja väljastab eraldi reale, mitu õuna jääb Lumivalgekesele.
+
+import random
+ounad = 14
+pp = int(input("Mitu PP tahab õuna: "))
+
+for i in range(pp):
+    oun = random.randint(1,2)
+    print(oun)
+    ounad -= oun
+
+print(f"Lumivalgekesele jäi {ounad} õuna")
+
+         
+
+
+
+
+                       
